86801754f9805be95f784201a6f24f6bfd64b8c7
Connor <cjm6653@rit.edu>
2023-02-16 23:45:17 -0500

    Entirely flushed out Product class for our E-store contains fields like 'id', 'name', 'price', etc. View documentation to see behavior, based off of Heros API from a few weeks ago.

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Product.java
@@ -0,0 +1,93 @@
+package com.estore.api.estoreapi.model;
+
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Product {
+    private static final Logger LOG = Logger.getLogger(Product.class.getName());
+
+    // private field members for the Product class
+    @JsonProperty("id") private int id;
+    @JsonProperty("name") private String name;
+    @JsonProperty("info") private String info;
+    @JsonProperty("price") private int price;
+    @JsonProperty("inStock") private Boolean inStock;
+
+    /**
+     * Create a product for the estore with given parameters.
+     * @param id The id of the product
+     * @param name The name of the product (fish)
+     * @param info Extra info abot product (habitat, size, lifespan, etc.)
+     * @param price Price of the product (fish)
+     * @param inStock If the fish is in stock or not
+     * 
+     * {@literal @}JsonProperty is used in serialization and deserialization
+     * of the JSON object to the Java object in mapping the fields.  If a field
+     * is not provided in the JSON object, the Java field gets the default Java
+     * value, i.e. 0 for int
+     */
+    public Product(@JsonProperty("id") int id, @JsonProperty("name") String name,
+    @JsonProperty("info") String info, @JsonProperty("price") int price,
+    @JsonProperty("inStock") Boolean inStock) {
+        this.id = id;
+        this.name = name;
+        this.info = info;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+    /**
+     * Retreives the id of the Product object
+     * @return The id of the Product 
+     */
+    public int getId() {return id;}
+
+    /**
+     * Retreives the name of the Product object
+     * @return The id name the Product 
+     */
+    public String getName() {return name;}
+
+    /**
+     * Retreives the info of the Product object
+     * @return The info of the Product 
+     */
+    public String getInfo() {return info;}
+
+    /**
+     * Retreives the price of the Product object
+     * @return The price of the Product 
+     */
+    public int getPrice() {return price;}
+
+    /**
+     * Retreives the inStock boolean of the Product object
+     * @return The inStock boolean of the Product 
+     */
+    public Boolean getInStock() {return inStock;}
+
+    /**
+     * Sets the name of the product
+     * @param name The new name of the product
+     */
+    public void setName(String name) {this.name = name;}
+
+    /**
+     * Sets the Price of the product
+     * @param price The new price of the product
+     */
+    public void setPrice(int price) {this.price = price;}
+
+    /**
+     * Sets the new info of the product
+     * @param info The new info of the product
+     */
+    public void setInfo(String info) {this.info = info;}
+
+    /**
+     * Sets the inStock boolean of the product
+     * @param inStock The new instock Boolean of the product
+     */
+    public void setInStock(Boolean inStock) {this.inStock = inStock;}
+}
