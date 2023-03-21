80f863747e2d6de28ea27506584de335a347afb3
Cristian Malone <cmm3351@rit.edu>
2023-03-21 02:57:21 -0400

    At this point, I have stubbed out the product.service.ts and products.component.ts classes

--- estore-ui/estore-ang/src/app/products/products.component.ts
+++ estore-ui/estore-ang/src/app/products/products.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts()
+    .subscribe(products => this.products = products);
+  }
+
+  add(name: string, info: string, price: number, quantity: number): void {
+    name = name.trim();
+    info = info.trim();
+    if (!name) { return; }
+    this.productService.addProduct({ name,info,price,quantity } as Product)
+      .subscribe(product => {
+        this.products.push(product);
+      });
+  }
+
+  delete(product: Product): void {
+    this.products = this.products.filter(p => p !== product);
+    this.productService.deleteProduct(product.id).subscribe();
+  }
 
 }
