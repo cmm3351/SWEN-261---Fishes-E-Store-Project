e9e7a42e26a17bf8a7908c56a88e6ab358c86344
Cristian Malone <cmm3351@rit.edu>
2023-04-24 00:20:16 -0400

    Cleaned up code in product.service.ts

--- estore-ui/estore-ang/src/app/product.service.ts
+++ estore-ui/estore-ang/src/app/product.service.ts
@@ -6,7 +6,6 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { User } from './user';
-//import { MessageService } from './message.service';
 
 
 @Injectable({ providedIn: 'root' })
@@ -21,37 +20,20 @@ export class ProductService {
 
   constructor(
     private http: HttpClient
-    //private messageService: MessageService
     ) { }
 
   /** GET products from the server */
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        //tap(_ => this.log('fetched products')),
         catchError(this.handleError<Product[]>('getProducts', []))
       );
   }
-
-  // /** GET product by id. Return `undefined` when id not found */
-  // getProduct1(id: number): Observable<Product> {
-  //   const url = `${this.productsUrl}/?id=${id}`;
-  //   return this.http.get<Product[]>(url)
-  //     .pipe(
-  //       map(products => products[0]), // returns a {0|1} element array
-  //       tap(p => {
-  //         const outcome = p ? 'fetched' : 'did not find';
-  //         //this.log(`${outcome} product id=${id}`);
-  //       }),
-  //       catchError(this.handleError<Product>(`getProduct id=${id}`))
-  //     );
-  // }
 
   /** GET product by id. Will 404 if id not found */
   getProduct(id: number): Observable<Product> {
     const url = `${this.productsUrl}/${id}`;
     return this.http.get<Product>(url).pipe(
-      //tap(_ => this.log(`fetched product id=${id}`)),
       catchError(this.handleError<Product>(`getProduct id=${id}`))
     );
   }
@@ -63,9 +45,6 @@ export class ProductService {
       return of([]);
     }
     return this.http.get<Product[]>(`${this.productsUrl}/?name=${term}`).pipe(
-      //tap(x => x.length ?
-         //this.log(`found products matching "${term}"`) :
-         //this.log(`no products matching "${term}"`)),
       catchError(this.handleError<Product[]>('searchProducts', []))
     );
   }
@@ -75,7 +54,6 @@ export class ProductService {
   /** POST: add a new product to the server */
   addProduct(product: Product): Observable<Product> {
     return this.http.post<Product>(this.productsUrl, product, this.httpOptions).pipe(
-      //tap((newProduct: Product) => this.log(`added product w/ id=${newProduct.id}`)),
       catchError(this.handleError<Product>('addProduct'))
     );
   }
@@ -85,7 +63,6 @@ export class ProductService {
     const url = `${this.productsUrl}/${id}`;
 
     return this.http.delete<Product>(url, this.httpOptions).pipe(
-      //tap(_ => this.log(`deleted product id=${id}`)),
       catchError(this.handleError<Product>('deleteProduct'))
     );
   }
@@ -93,7 +70,6 @@ export class ProductService {
   /** PUT: update the product on the server */
   updateProduct(product: Product): Observable<any> {
     return this.http.put(this.productsUrl, product, this.httpOptions).pipe(
-      //tap(_ => this.log(`updated product id=${product.id}`)),
       catchError(this.handleError<any>('updateProduct'))
     );
   }
@@ -141,16 +117,8 @@ export class ProductService {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      //this.log(`${operation} failed: ${error.message}`);
-
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
-
-  // /** Log a HeroService message with the MessageService */
-  // private log(message: string) {
-  //   this.messageService.add(`HeroService: ${message}`);
-  // }
 }
